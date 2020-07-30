35345bd2237f38b97675fb0174b59578e164479b
Chris <chrissx@chrissx.de>
2020-07-30 23:35:24 +0200

    better testing (no stupid hacks) using python

---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from subprocess import Popen
+from socket import *
+
+tcp = Popen(['./tcp', 'This is a test purely intended to test.'])
+udp = Popen(['./udp', 'This is a test purely intended to test.'])
+
+sock = socket(AF_INET, SOCK_STREAM)
+sock.connect(('127.0.0.1', 17))
+print(sock.recv(512).decode('UTF-8'), end='')
+sock.close()
+
+sock = socket(AF_INET, SOCK_DGRAM)
+sock.sendto(b'\0'*22, ('127.0.0.1', 17))
+print(sock.recv(512).decode('UTF-8'), end='')
+sock.close()
+
+tcp.kill()
+udp.kill()
