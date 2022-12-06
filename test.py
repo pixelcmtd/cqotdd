3753cc98f815686077f8ec6c7f0a282f6693874b
pixel <pixel@chrissx.de>
2022-12-06 01:00:07 +0100

    more robust and detailed test script

--- test.py
+++ test.py
@@ -2,19 +2,35 @@
 
 from subprocess import Popen
 from socket import *
+from time import sleep
 
-tcp = Popen(['./cqotdd-tcp', 'This is a test purely intended to test.'])
-udp = Popen(['./cqotdd-udp', 'This is a test purely intended to test.'])
+tcp = Popen(
+    ['./cqotdd-tcp', 'This message should be wholly displayed if TCP works.'])
+udp = Popen([
+    './cqotdd-udp',
+    (s := 'This message should be visible twice if UDP works.\n'
+     'If this message is visible twice, we are a DDoS amplifier.\n'
+     'If there is anything other than spaces after this message, we have a security problem.'
+     ) + (511 - len(s)) * ' '
+])
 
-sock = socket(AF_INET, SOCK_STREAM)
-sock.connect(('127.0.0.1', 17))
-print(sock.recv(512).decode('UTF-8'), end='')
-sock.close()
+try:
+    sleep(0.25)
 
-sock = socket(AF_INET, SOCK_DGRAM)
-sock.sendto(b'\0'*22, ('127.0.0.1', 17))
-print(sock.recv(512).decode('UTF-8'), end='')
-sock.close()
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.connect(('127.0.0.1', 17))
+    print(sock.recv(512).decode('UTF-8'), end='')
+    sock.close()
 
-tcp.kill()
-udp.kill()
+    sock = socket(AF_INET, SOCK_DGRAM)
+    sock.sendto(b'\0' * 51, ('127.0.0.1', 17))
+    print(sock.recv(512).decode('UTF-8'), end='')
+    sock.close()
+
+    sock = socket(AF_INET, SOCK_DGRAM)
+    sock.sendto(b'\0' * 600, ('127.0.0.1', 17))
+    print(sock.recv(600).decode('UTF-8'), end='')
+    sock.close()
+finally:
+    tcp.kill()
+    udp.kill()
